10395fef8422be467c77076b1bf5b8fc56633ff7
Hanife Ceylan <hanifeceylan064@gmail.com>
2022-04-28 23:55:43 +0300

    28/04/22

--- src/HastaneRunner.java
+++ src/HastaneRunner.java
@@ -1,19 +1,58 @@
-public class HastaneRunner implements Method{
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-    private static Hastane hastane1= new Hastane();
+public class HastaneRunner implements Method {
 
-    static Durum obj= new Durum();
+    private static Hastane hastane1 = new Hastane();
+
+    static Durum obj = new Durum();
+    static Map<String, String> doktorListesi = new HashMap<>();
+    static Map<Integer, String> hastaListesi = new HashMap<>();
+    static String hastaDurumu;
+   static  String unvan;
+    static Scanner scan =new Scanner(System.in);
 
 
     public static void main(String[] args) {
-        String hastaDurumu;
-        String unvan;
+
+
+
+        doktorListesi.put("Allergist", "Nilson, Avery ");
+        doktorListesi.put("Norolog", "John, Abel ");
+        doktorListesi.put("Genel cerrah", "Robert, Erik ");
+        doktorListesi.put("Cocuk doktoru", "Marry, Jacob ");
+        doktorListesi.put("Dahiliye", "Alan, Pedro ");
+        doktorListesi.put("Kardiyalog", "Mahesh, Tristen ");
+
+
+        hastaListesi.put(111, "Warren, Traven, Allerji");
+        hastaListesi.put(222, "Petanow, William, Bas Agrisi");
+        hastaListesi.put(333, "Sophia, George, Diabet");
+        hastaListesi.put(444, "Emma, Tristen, Sogukalginligi");
+        hastaListesi.put(555, "Darian, Luis, Migren");
+        hastaListesi.put(666, "Peter, Cole, Kalphastaliklari");
+
 
 
     }
 
     @Override
-    public void doktorUnvani() {
+    public  void doktorUnvani() {
+        Doktor doktor = new Doktor();
+        System.out.println(" lutfen gitmek istediginiz bolum yaziniz ");
+        String bolum= scan.nextLine().toUpperCase();
+
+        for (Map.Entry<String, String> each : doktorListesi.entrySet()) {
+            if(doktorListesi.containsKey(bolum)) {
+
+                System.out.println(" istediginiz bolumde doktor bulunmaktadir");
+
+            }else{
+                System.out.println(" istediginiz bolumde doktor bulunmamakladir");
+            }
+        }
+
 
 
     }
@@ -26,6 +65,7 @@ public class HastaneRunner implements Method{
     @Override
     public void doktorBul() {
 
+
     }
 
     @Override
@@ -33,3 +73,4 @@ public class HastaneRunner implements Method{
 
     }
 }
+
